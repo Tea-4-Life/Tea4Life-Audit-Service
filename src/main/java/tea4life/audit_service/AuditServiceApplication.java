cfab24c46a8e2d4378d0372f8a4d97476c447a33
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-03-19 17:25:27 +0700

    refactor(audit-service): Remove JpaAuditing and FeignClients
    This commit removes several components related to JPA auditing, Feign clients, and custom ID generation. The AuditServiceApplication.java was updated to reflect these removals, no longer enabling JpaAuditing or FeignClients. Corresponding configuration files and base entities (BaseEntity.java, FeignClientConfig.java, JpaAuditing.java, IdConfig.java, SnowflakeGenerated.java, SnowflakeIdGenerator.java) have been deleted. Additionally, the pom.xml was updated, removing spring-cloud-starter-openfeign and spring-boot-starter-kafka dependencies, and adjusting the spring-cloud-starter-netflix-eureka-client dependency. The .gitkeep file in utils was also removed as it is no longer needed. This indicates a shift in the service's architecture, potentially moving away from JPA-based auditing and Feign for inter-service communication.

--- src/main/java/tea4life/audit_service/AuditServiceApplication.java
+++ src/main/java/tea4life/audit_service/AuditServiceApplication.java
@@ -2,15 +2,11 @@ package tea4life.audit_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableWebSecurity
-@EnableJpaAuditing(auditorAwareRef = "auditorAware")
-@EnableFeignClients
 @EnableMethodSecurity
 //TRIGGER: 2024-06-01 12:00:00
 public class AuditServiceApplication {
