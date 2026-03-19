34ba2904e9fff3d7f2b887949bffb21e3ea316eb
phatdang <phatdang19052004@gmail.com>
2026-03-19 17:06:36 +0700

    feat(model.enums): Add ProductDeletedAuditEvent and ProductAuditConsumer for product deletion events

---
+++ src/main/java/tea4life/audit_service/model/enums/EntityType.java
@@ -0,0 +1,14 @@
+package tea4life.audit_service.model.enums;
+
+/**
+ * @author : user664dntp
+ * @mailto : phatdang19052004@gmail.com
+ * @created : 19/03/2026, Thursday
+ **/
+public enum EntityType {
+    PRODUCT,
+    CATEGORY,
+    USER,
+    ORDER,
+    SYSTEM
+}
