2bce2ab24a718ade4c53202db20796baecce3442
phatdang <phatdang19052004@gmail.com>
2026-03-19 17:05:51 +0700

    feat(model.enums): Implement audit logging service and related entities

---
+++ src/main/java/tea4life/audit_service/model/enums/AuditAction.java
@@ -0,0 +1,12 @@
+package tea4life.audit_service.model.enums;
+
+/**
+ * @author : user664dntp
+ * @mailto : phatdang19052004@gmail.com
+ * @created : 19/03/2026, Thursday
+ **/
+public enum AuditAction {
+    CREATE,
+    UPDATE,
+    DELETE
+}
